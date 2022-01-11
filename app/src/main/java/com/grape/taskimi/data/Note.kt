e034f10387c0da1e7c2da64cdf9bb61912b4f418
Moe <Mohammed27Zalzala11@gmail.com>
2022-01-11 03:50:56 +0300

    edit the xml layout

--- app/src/main/java/com/grape/taskimi/data/Note.kt
+++ app/src/main/java/com/grape/taskimi/data/Note.kt
@@ -3,9 +3,10 @@ package com.grape.taskimi.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.util.*
 
 @Entity(tableName = "Note_Table")
-data class Note (
+data class Note(
     @PrimaryKey(autoGenerate = true) val id:Long,
     @ColumnInfo(name = "Content") val content: String,
     @ColumnInfo(name = "Date") val date: String,
--- app/src/main/java/com/grape/taskimi/ui/home/NotesViewModel.kt
+++ app/src/main/java/com/grape/taskimi/ui/home/NotesViewModel.kt
@@ -1,16 +1,15 @@
 package com.grape.taskimi.ui.home
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.grape.taskimi.data.Note
 import com.grape.taskimi.data.repository.NoteRepository
-import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
-import io.reactivex.rxjava3.schedulers.Schedulers
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
+import java.time.LocalDate
+import java.util.*
 
 class NotesViewModel : ViewModel(), NoteInteractionListener {
 
@@ -28,7 +27,7 @@ class NotesViewModel : ViewModel(), NoteInteractionListener {
     fun addNote(){
         viewModelScope.launch {
             newNotesText.value?.let {
-                repository.insertNewNote(Note(0, it, "2021/1/1", false))
+                repository.insertNewNote(Note(0, it,"2021/1/1", false))
                 }
             }
         }
